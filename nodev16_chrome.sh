e03489390af8a0dce6a20e247ae05bee1b5df33f
Caua Silva <149310616+CauaCodeDev@users.noreply.github.com>
2023-11-11 00:44:11 -0400

    Update nodev16_chrome.sh

--- nodev16_chrome.sh
+++ nodev16_chrome.sh
@@ -12,7 +12,7 @@ dpkg-reconfigure --frontend noninteractive tzdata > /dev/null 2>&1
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 nvm install 16
