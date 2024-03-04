f62df07b68b655822e2250b44f2636043c581b56
Caua Silva <149310616+CauaCodeDev@users.noreply.github.com>
2024-03-04 19:58:07 -0400

    Update ativar_senha_root.sh

--- ativar_senha_root.sh
+++ ativar_senha_root.sh
@@ -1,33 +1,31 @@
 #!/bin/bash
 clear
 
-echo -ne "\033[1;32mDigite sua nova senha root\033[1;37m: "; read senha
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Este script precisa ser executado como root."
+  exit 1
+fi
+
+echo -ne "\033[1;32mDigite a nova senha para o usuário root: \033[1;37m"
+read -s senha
+echo
 
 if [ -z "$senha" ]; then
-   echo "Senha vazia, defina uma senha valida!"
+   echo "A senha não pode estar vazia. Por favor, forneça uma senha válida."
    exit 1
 fi
 
- $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0'  && {
-	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
-} > /dev/null
- $(grep -c "without-password" /etc/ssh/sshd_config) != '0'  && {
-	sed -i "s/without-password/yes/g" /etc/ssh/sshd_config
-} > /dev/null
- $(grep -c "#PermitRootLogin" /etc/ssh/sshd_config) != '0'  && {
-	sed -i "s/#PermitRootLogin/PermitRootLogin/g" /etc/ssh/sshd_config
-} > /dev/null
- $(grep -c "PasswordAuthentication" /etc/ssh/sshd_config) = '0'  && {
-	echo 'PasswordAuthentication yes' > /etc/ssh/sshd_config
-} > /dev/null
- $(grep -c "PasswordAuthentication no" /etc/ssh/sshd_config) != '0'  && {
-	sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-} > /dev/null
- $(grep -c "#PasswordAuthentication no" /etc/ssh/sshd_config) != '0'  && {
-	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-} > /dev/null
+echo "root:$senha" | chpasswd
 
-service ssh restart > /dev/null
+if grep -q "#PermitRootLogin" /etc/ssh/sshd_config; then
+    sed -i 's/#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+fi
+
+if ! grep -q "PasswordAuthentication yes" /etc/ssh/sshd_config; then
+    sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+fi
+
+systemctl restart ssh
 
 iptables -F
 iptables -A INPUT -p tcp --dport 81 -j ACCEPT
@@ -36,6 +34,4 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
-
-echo "root:$senha" | chpasswd
 clear
